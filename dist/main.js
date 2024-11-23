84a2669cc5a9d0da6695233fad33acbb3a76aab4
m-suzuki <msuzuki0803+github@gmail.com>
2024-11-23 16:21:29 +0900

    feat: ssl

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -12,11 +35,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const config_1 = require("@nestjs/config");
+const fs = __importStar(require("fs"));
+const path = __importStar(require("path"));
+const sslOptions = {
+    key: fs.readFileSync(path.join(__dirname, '../ssl/private.key')),
+    cert: fs.readFileSync(path.join(__dirname, '../ssl/term-app.crt')),
+    ca: fs.readFileSync(path.join(__dirname, '../ssl/term-app.ca-bundle')),
+};
 function bootstrap() {
     return __awaiter(this, void 0, void 0, function* () {
-        const app = yield core_1.NestFactory.create(app_module_1.AppModule);
+        const app = yield core_1.NestFactory.create(app_module_1.AppModule, {
+            httpsOptions: sslOptions,
+        });
         const configService = app.get(config_1.ConfigService);
-        const port = configService.get('PORT') || 8080;
+        const port = configService.get('PORT') || 443;
         yield app.listen(port);
         console.log(`Server running on port ${port}`);
     });
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const sslOptions = {
+    key: fs.readFileSync(path.join(__dirname, '../ssl/private.key')),
+    cert: fs.readFileSync(path.join(__dirname, '../ssl/term-app.crt')),
+    ca: fs.readFileSync(path.join(__dirname, '../ssl/term-app.ca-bundle')),
+};
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {
+        httpsOptions: sslOptions,
+    });
     const configService = app.get(ConfigService);
-    const port = configService.get<number>('PORT') || 8080;
+    const port = configService.get<number>('PORT') || 443;
     await app.listen(port);
     console.log(`Server running on port ${port}`);
 }
